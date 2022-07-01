a1dfca7b807ed36834250450492bf9500a60cdd9
lis76 <rsavoskin@yandex.ru>
2022-07-01 10:22:10 +0300

    Установка Devises

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://desolate-savannah-45088.herokuapp.com', port: 3000 }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
