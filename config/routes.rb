664abf8d24015d2715a9878d577403dbea049634
lis76 <rsavoskin@yandex.ru>
2022-07-01 12:10:17 +0300

    Настройка авторизации

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'pages#landing'
   get 'pages/about'
 end
