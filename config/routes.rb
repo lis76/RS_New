31c5582f2ae9ee7dddeb9545b9032a0adcb82f0a
lis76 <rsavoskin@yandex.ru>
2022-07-01 14:06:20 +0300

    Pravka

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   devise_for :users
+
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get "/users/sign_out" => "devise/sessions#destroy"
   end
-  root 'pages#landing'
-  get 'pages/about'
+  root "pages#landing"
+  get "pages/about"
 end
