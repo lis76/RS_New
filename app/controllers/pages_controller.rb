f63ca109f60a8508626c7705cc75746f514d9577
lis76 <rsavoskin@yandex.ru>
2022-07-07 10:29:46 +0300

    Создана ассоциация пользователя и фото

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def landing; end
+  def landing
+    redirect_to '/photos' if user_signed_in?
+  end
+
   def about; end
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,67 +1,60 @@
 class PhotosController < ApplicationController
-  before_action :set_photo, only: %i[ show edit update destroy ]
+  before_action :set_photo, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :owner, only: %i[edit update destroy]
 
-  # GET /photos or /photos.json
   def index
     @photos = Photo.all
   end
 
-  # GET /photos/1 or /photos/1.json
   def show; end
 
-  # GET /photos/new
   def new
     @photo = current_user.photos.build
   end
 
-  # GET /photos/1/edit
   def edit; end
 
-  # POST /photos or /photos.json
   def create
     @photo = current_user.photos.build(photo_params)
 
     respond_to do |format|
       if @photo.save
         format.html { redirect_to photo_url(@photo), notice: 'Photo was successfully created.' }
-        format.json { render :show, status: :created, location: @photo }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
-      end
+        format.html { render :new, status: :unprocessable_entity }      end
     end
   end
 
-  # PATCH/PUT /photos/1 or /photos/1.json
   def update
     respond_to do |format|
       if @photo.update(photo_params)
         format.html { redirect_to photo_url(@photo), notice: 'Photo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @photo }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /photos/1 or /photos/1.json
   def destroy
     @photo.destroy
 
     respond_to do |format|
       format.html { redirect_to photos_url, notice: 'Photo was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
+  def authenticate_user!; end
+
+  def owner
+  end
+
   def set_photo
     @photo = Photo.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def photo_params
     params.require(:photo).permit(:description)
   end
