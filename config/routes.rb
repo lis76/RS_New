2f1dbf7793e4e24c1cdf4ceea52b3adc84750b1b
lis76 <rsavoskin@yandex.ru>
2022-07-20 11:18:29 +0300

    Настройка Подписок пользователя

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   root 'pages#landing'
   get 'pages/about'
   get '/profiles/:id' => 'profiles#show', as: 'profile'
+  get '/profiles/:id/subscribe' => 'profiles#subscribe', as: 'subscribe_profile'
+  get '/profiles/:id/unsubscribe' => 'profiles#unsubscribe', as: 'unsubscribe_profile'
 end
